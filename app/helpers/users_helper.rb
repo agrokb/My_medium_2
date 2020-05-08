d5dd99095044b84ab817e99623b18feee346e664
larry <dh821020@gmail.com>
2020-05-09 00:39:49 +0900

    Story Image List N+1 Problem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,5 @@
 module UsersHelper
-
     def avatar(user,size: 50)
-         image_tag user.avatar.variant(combine_options:{resize:"640x640^",crop:"640x640+0+0",gravity: :center}).processed,class: 'user-avatar' if current_user.avatar.attached? 
+        image_tag user.avatar.variant(combine_options:{resize:"#{size}x#{size}^",crop:"#{size}x#{size}+0+0",gravity: :center}).processed,class: 'user-avatar' if current_user.avatar.attached? 
     end
-
 end
