e06708b3f5a3834e19e9edbe55555ad8945464b9
larry <dh821020@gmail.com>
2020-05-17 15:30:15 +0900

    Publish migration fixed

--- db/migrate/20200512111650_add_counter_to_story.rb
+++ db/migrate/20200512111650_add_counter_to_story.rb
@@ -1,5 +1,5 @@
 class AddCounterToStory < ActiveRecord::Migration[6.0]
   def change
-    add_column :stories, :clap, :interger,default:0
+    add_column :stories, :clap, :integer,default:0
   end
 end
